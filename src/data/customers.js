e00e3ce633485f4502e739b3cccd2891d3f60b89
OluC94 <coker.olu94@gmail.com>
2024-08-07 13:40:46 +0100

    create starting data

---
+++ src/data/customers.js
@@ -0,0 +1,26 @@
+export const customers = [
+    {
+        id: 1,
+        name: "Ali",
+        stampCount: 3,
+        coffeeCount: 0,
+    },
+    {
+        id: 2,
+        name: "Bilal",
+        stampCount: 5,
+        coffeeCount: 0,
+    },
+    {
+        id: 3,
+        name: "Charlie",
+        stampCount: 0,
+        coffeeCount: 2,
+    },
+    {
+        id: 4,
+        name: "Dani",
+        stampCount: 3,
+        coffeeCount: 1,
+    },
+];
